ed78affae40863f8e93d41ba897e3ab8fa925987
torong <hi_ryuji@naver.com>
2024-05-11 14:03:12 +0900

    feat: propTypes를 통한 props 검증
    - isRequired를 사용하여 필수 propTypes 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import MyComponent from "./MyComponent"; // 모듈 불러오기
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return (
+    <MyComponent name="React" favoriteNumber={18}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
