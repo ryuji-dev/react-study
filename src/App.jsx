11433326edfe6ddb950d83fd30daebe752b8891c
torong <hi_ryuji@naver.com>
2024-05-17 16:57:04 +0900

    feat: 콜백 함수를 통한 ref 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import EventPractice from "./EventPractice";
+import { Component } from "react";
+import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
 
 export default App;
