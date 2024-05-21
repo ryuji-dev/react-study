fe894ef3a594963b3ab7544c0c4f02030e7bab5f
torong <hi_ryuji@naver.com>
2024-05-22 00:33:48 +0900

    feat: IterationSample 컴포넌트 추가 1. 데이터 추가 기능 2. 데이터 제거 기능

--- src/IterationSample.jsx
+++ src/IterationSample.jsx
@@ -1,15 +1,49 @@
-const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  /*
-    const nameList = names.map((name) => <li>{name}</li>);
+import { useState } from "react";
 
-    위 코드는 오류가 남
-    이 오류는 map 함수를 사용하여 요소를 생성할 때, 각 요소에 고유한 key 속성을 추가하지 않았기 때문에 발생함
-    React는 리스트의 각 항목을 구별하기 위해 key 속성을 필요함
-    아래는 수정된 코드다.
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const onChange = (e) => setInputText(e.target.value);
+
+  /*
+    왜 push 함수를 사용하지 않고 concat을 사용할까?
+    push 함수는 기존 배열 자체를 변경해주지만, concat은 새로운 배열을 만들어 주기 때문
   */
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+
+    setNextId(nextId + 1); // nextId 값에 1을 더함
+    setNames(nextNames); // names 값 업데이트
+    setInputText(""); // inputText를 비움
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const namesList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
