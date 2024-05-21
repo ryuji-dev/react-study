ea991fd6f7e2e7e2162a44c437eb01ff91be472e
torong <hi_ryuji@naver.com>
2024-05-21 23:44:06 +0900

    feat: 컴포넌트에 ref 달고 내부 메서드 사용 - 맨 밑으로 버튼을 눌러 스크롤바를 맨 아래쪽으로 이동시키기

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
-    return <ScrollBox />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
   }
 }
 
--- src/ScrollBox.jsx
+++ src/ScrollBox.jsx
@@ -1,6 +1,16 @@
 import { Component } from "react";
 
 class ScrollBox extends Component {
+  scrollToBottom = () => {
+    /* 
+        scrollTop: 세로 스크롤바 위치
+        scrollHeight: 스크롤이 있는 박스 안의 div 높이 
+        clientHeight: 스크롤이 있는 박스의 높이
+     */
+    const { scrollHeight, clientHeight } = this.box; // 비구조화 할당 문법
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
+
   render() {
     const style = {
       border: "1px solid black",
